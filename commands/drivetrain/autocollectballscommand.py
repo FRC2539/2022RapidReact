a4b9dc4fea71cd31cfdf176f4a00d8eb96c8d2d8
Krypton Cougar <kryptoncougars2539@gmail.com>
2022-02-14 20:19:21 -0500

    Temp testing

--- commands/drivetrain/autocollectballscommand.py
+++ commands/drivetrain/autocollectballscommand.py
@@ -2,6 +2,8 @@ from commands2 import CommandBase
 
 import robot
 
+from constants import drivetrain as drivetrainConstants
+
 from wpimath.geometry import Translation2d, Rotation2d, Pose2d
 from wpimath.kinematics import ChassisSpeeds
 import constants
@@ -11,40 +13,55 @@ import math
 class AutoCollectBallsCommand(CommandBase):
     def __init__(self):
         super().__init__()
-        # self.addRequirements(robot.drivetrain)
-
-        # Store the current screen resolution.
-        # self.resolutionX = screenResolution[0]
-        # self.resolutionY = screenResolution[1]
-
-    # def initialize(self):
+        self.addRequirements(robot.drivetrain)
+        
+        self.reactionSpeed = 1
+        self.maxRotationSpeed = drivetrainConstants.angularSpeedLimit
+        self.minRotationSpeed = drivetrainConstants.angularSpeedMinimum
+        
+        self.radianTolerance = 0.1
+        
+        
+    def initialize(self):
+        pass
 
     def execute(self):
-        # Update the drivetrain to have it match the desired rotational velocity (omega)
-        # Note: the desired rotational velocity comes from the pid controller (CHANGE)
-
-        # Adjust the  the robot angle.
-        # robot.drivetrain.setChassisSpeeds(
-        #     ChassisSpeeds(0, 0, self.pidController.calculate(self.estimateBallPose()))
-        # )
+        robot.drivetrain.setChassisSpeeds(
+            ChassisSpeeds(calcForwardVelocity(), 0, calcRotationSpeed())
+        )
         print("target x:" + str(robot.ml.getX()))
+    
 
-    # def isFinished(self):
-    #     return (
-    #         self.pidController.atGoal()
-    #         or not robot.drivetrain.isVisionTargetDetected()  # Does not exist yet.
-    #     )
-
-    # def end(self, interrupted):
-    #     robot.drivetrain.stop()
-
-    # def calculateBallHorizontalDistance(self):
-    #     """
-    #     Calculates the approximate distance to the target ball.
-    #     Notes:
-    #     The camera constants must be correct (camera height, angle, target ball offset).
-    #     """
-
-    #     targetBallPosition = (constants.drivetrain.resolutionX / 2) + 0  # targetX
-
-    #     return constants.limelight.heightOffset / math.tan(totalAngle)
+    def end(self, interrupted):
+        robot.drivetrain.stop()
+    
+    def calcForwardVelocity(self):
+        return 0
+    
+    def calcRotationSpeed(self):
+        if robot.ml.isTargetAcquired():
+            return 0
+        
+        velocity = getXNormalized() * self.reactionSpeed
+        
+        absVel = abs(velocity)
+        
+        absVel = max(absVel,self.minRotationSpeed)
+        absVel = min(absVel,self.maxRotationSpeed)
+        
+        if abs(getXNormalized()) <= self.radianTolerance:
+            absVel = 0
+        
+        velocity = math.copysign(absVel, velocity)
+        
+        return velocity
+        
+    def getXNormalized(self):
+        """Returns a value between -1 (left) and 1 (right) for where the ball is on the x axis"""
+        xPosition = robot.ml.getX()
+        xResolution = robot.ml.getResX()
+        
+        #normalizes and then moves 
+        xNormalized = xPosition/xResolution * 2 - 1
+        
+        return xNormalized
